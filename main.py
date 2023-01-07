462d7d7fc465c1a56979bd7ac414ce3f5d898c41
Ayushi Roy <109765650+royayushi@users.noreply.github.com>
2023-01-07 11:16:27 +0000

    Added database, and modified a lot of files.

--- main.py
+++ main.py
@@ -2,26 +2,35 @@ from flask import Flask, request, flash, url_for, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Userinfo.sqlite3'
 app.config['SECRET_KEY'] = "The water is red"
 
 db = SQLAlchemy(app)
-class userinfo(db.Model):
-   id = db.Column('user_ph', db.Integer, primary_key = True)
-   fname = db.Column(db.String(100))
-   lname = db.Column(db.String(100))
+
+
+class Userinfo(db.Model):
+    __tablename__ = "userinfo"
+    id = db.Column('user_ph', db.Integer, primary_key=True)
+    fname = db.Column(db.String(100))
+    lname = db.Column(db.String(100))
+
+    def __repr__(self):
+        return f'<Userinfo {self.fname, self.lname}>'
+
 
 def __init__(self, fname, lname):
-   self.fname = fname
-   self.lname = lname
+    self.fname = fname
+    self.lname = lname
+
 
 @app.route('/')
-def enter():
-    return render_template('enter.html')
+def index():
+    return render_template('index.html')
 
-@app.route('/enter', methods=('GET', 'POST'))
-def create():
-  if request.method == 'POST':
+
+@app.route('/enter/', methods=('GET', 'POST'))
+def enter():
+    if request.method == 'POST':
         fname = request.form['fname']
         lname = request.form['lname']
         phno = request.form['phno']
@@ -29,16 +38,12 @@ def create():
         if not phno or not fname or not lname:
             flash('Please enter all the fields', 'error')
         else:
-            user = userinfo(fname, lname, phno)
-         
+            user = Userinfo(id=phno, fname=fname, lname=lname)
             db.session.add(user)
             db.session.commit()
-            flash('Record was successfully added')
-            return redirect(url_for('enter'))
-  return render_template('enter.html')
+            return redirect(url_for('index'))
+    return render_template('enter.html')
 
-with app.app_context():
-  if __name__ == '__main__':
-    db.create_all()
-  
+
+
 app.run(host='0.0.0.0', port=81)
